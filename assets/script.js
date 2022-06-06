60b747d0900d87752b2353ddcb1e9ddb9dc2c968
HarrisonHayes <ha.hayes@yahoo.com>
2022-06-06 11:06:35 -0400

    added query selectors for swSearchBtn, swSearchTxt, and swShowToolTip

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,4 @@
 var populationValue = document.querySelector("#maxPopulation");
-
+var swSearchBtn = document.querySelector("#sw-searchBtn");
+var swSearchTxt = document.querySelector("#sw-searchInput");
+var swTooltip = document.querySelector("sw-searchInput");

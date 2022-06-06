d41f24a3e57c38ae8ebfe2a2e4e0e5fecca28117
VanessaLiaw021 <vliaw021@gmail.com>
2022-06-06 14:09:21 -0400

    made minor changes to display to html

--- assets/script.js
+++ assets/script.js
@@ -13,16 +13,16 @@ function getCountry() {
     // first api call (REST Countries)
     var data = restCountryData;
     data = data[0];
-    // var restCountryURL = `https://restcountries.com/v3.1/capital/${data.capital}`
-    // second api call (REST Countries/Capital)
-    data = restCountryCapitalData;
-    data = data[0];
     countryData.captial = data.capital;
     countryData.language = data.languages[0].name;
     countryData.timeZones = data.timezones[0];
     countryData.population = data.population;
     countryData.continents = data.continent;
     countryData.currency = data.currencies[0].name;
+    // var restCountryURL = `https://restcountries.com/v3.1/capital/${data.capital}`
+    // second api call (REST Countries/Capital)
+    data = restCountryCapitalData;
+    data = data[0];
     // var weatherURL = `https://api.openweathermap.org/data/2.5/onecall?lat=${data.capitalInfo.latlng[0]}&lon=${data.capitalInfo.latlng[1]}&exclude=daily,minutely,hourly&appid=${weatherAPIKey}`
     // third api call (OpenWeatherMap)
     data = weatherData;
@@ -38,6 +38,8 @@ function getCountry() {
 }
 
 function displayCountry(data) {
+
+    console.log(data);
 
     //Query Selector for display country info
     var capital = $("#capital");
